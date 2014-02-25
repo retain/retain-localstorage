250b3729f0ae61c262ecb484f7ef102e05b2a90c
Giulian Drimba <giuliandrimba@hotmail.com>
2014-02-24 22:58:24 -0300

    Set up tests

---
+++ test/spec/test.js
@@ -0,0 +1,13 @@
+var retain = require("retain");
+var server = require("../fixtures/server")
+var assert = require("chai").assert;
+
+
+describe("RetainAjax", function()
+{
+  before(function()
+  {
+    server.start();
+  });
+
+});
