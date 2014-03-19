16ae1d24a5159da99d350c471d25fa6b66fe2194
Giulian Drimba <giuliandrimba@hotmail.com>
2014-03-19 00:42:14 -0300

    Write API tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -14,19 +14,70 @@ describe("RetainLocalStorage", function()
 
   it("it should add retain-localstorage as a plugin", function(done)
   {
-    Movies.use(retainLocalStorage, {})
+    Movies.use(retainLocalStorage, {name:"movies"});
     done();
   })
   
+  it("it should create a new movie", function(done)
+  {
+    var PI = Movies.new(function(res, err)
+      {
+        if(res)
+        {
+          done();
+        }
+      });
+  });
+
   it("it should get all the movies", function(done)
   {
-    Movies.all(function(res, err)
+    var movie = Movies.all(function(res, err)
     {
       if(res)
       {
         done();
       }
+
     });
-  });
+
+  })
+
+  it("it should find the created movie", function(done)
+  {
+    var movie = Movies.find(1,function(res, err)
+    {
+      if(res)
+      {
+        done();
+      }
+
+    });
+  })
+
+  it("it should set the movie properties", function(done)
+  {
+    var movie = Movies.find(1);
+    movie.set({name:"PI", watched:true},function(res, err)
+    {
+      if(res)
+      {
+        done();
+      }
+
+    });
+  })
+
+  it("it should remove the movie", function(done)
+  {
+    var movie = Movies.find(1);
+    movie.remove(function(res, err)
+    {
+      if(res)
+      {
+        done();
+      }
+
+    });
+  })
 
 });
